6f35fa17670dcc50929ceb7fba7e90d8261cea14
arsen-senyk <arsen.senyk@gmail.com>
2017-10-23 15:57:21 +0300

    deleted some packages

--- src/test/Gmail/pages/data_base.java
+++
@@ -1,8 +0,0 @@
-package pages;
-
-public class data_base {
-
-    public String url_gmail = "https://mail.google.com/mail/u/0/#inbox_page";
-    public String login = "arsen.senyk@gmail.com";
-    public String password = "burunduk20107";
-}
